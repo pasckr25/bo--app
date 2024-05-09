4506285c5e29d8f65ebb9aef6c9f12975f6ad412
pasckr25 <167246208+pasckr25@users.noreply.github.com>
2024-05-09 16:07:36 +0000

    test

--- Hello.py
+++ Hello.py
@@ -65,25 +65,7 @@ if st.session_state['result_counter']<10:
          #   st.dataframe(df_result.head())
 else:
     st.write("Congrats! Thanks ", user," for playing, go ahead and check your results.")
-    if st.button("Submit your results"):
-        # Create a connection object.
-        credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"],scopes=["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"],)
-        #conn = connect(credentials=credentials)
-        client=gspread.authorize(credentials)
 
-        sheet_id = '1noIa_2NECsdUPp087oSCDqcj1PP6IzDlbTx1RdSuaHg'
-        csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
-        database_df = pd.read_csv(csv_url, on_bad_lines='skip')
-
-    
-        database_df=pd.concat([database_df,df_results],ignore_index=True)
-        st.dataframe(database_df)
-
-        #database_df = database_df.astype(str)
-        #sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
-        #sheet = client.open_by_url(sheet_url).sheet1
-        #sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
-        #st.success('Data has been written to Google Sheets')
 
 
 
@@ -99,5 +81,22 @@ with c_plot_left:
 with c_plot_right:
     st.line_chart(df_results["yield"])
 
-#if st.session_state['result_counter']>9:
-    #df_results.to_excel("results.xlsx")
+if st.session_state['result_counter']>9:
+    if st.button("Submit your results"):
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"],scopes=["https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive"],)
+        #conn = connect(credentials=credentials)
+        client=gspread.authorize(credentials)
+
+        sheet_id = '1noIa_2NECsdUPp087oSCDqcj1PP6IzDlbTx1RdSuaHg'
+        csv_url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+        database_df = pd.read_csv(csv_url, on_bad_lines='skip')
+
+
+        database_df=pd.concat([database_df,df_results],ignore_index=True)
+
+        database_df = database_df.astype(str)
+        sheet_url = st.secrets["private_gsheets_url"] #this information should be included in streamlit secret
+        sheet = client.open_by_url(sheet_url).sheet1
+        sheet.update([database_df.columns.values.tolist()] + database_df.values.tolist())
+        st.success('Data has been written to Google Sheets')
